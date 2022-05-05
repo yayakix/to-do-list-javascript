a31efca462077d3d01b6087a18ac818d5d5a69b0
Iyana Marquez <yayakix@gmail.com>
2022-05-04 21:14:13 -0400

    working add and delete buttons

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+$("button").click(function(){
+
+    let text = $("input").val()
+    let liEl = document.createElement("li")
+    liEl.innerHTML = `<i class="fa-solid fa-x fa-xs"></i>${text}`;
+    document.querySelector(".ulist").appendChild(liEl)
+})
+
+const list = document.querySelectorAll("ul");
+list.forEach(x => x.addEventListener('click', function(event){
+const element = event.target
+let iTag = element.tagName.toLowerCase()
+if (iTag == "i"){
+    element.parentNode.remove()
+}
+})
+)
+
+
+// $("li").click(function () {
+   
+//     // $(this).remove()
+
+//     // $(this).parent().remove()
+//     // $("li").eq(index).remove()
+//     alert("test")
+// });
+
