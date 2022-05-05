c12bde573ba07934366a0f7f7b63aac71e1653ef
Iyana Marquez <yayakix@gmail.com>
2022-05-05 09:22:19 -0400

    clear input on submit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
-
-
-$("button").click(function(){
-
-    let text = $("input").val()
-    let liEl = document.createElement("li")
-    liEl.innerHTML = `<i class="fa-solid fa-x fa-xs"></i>${text}`;
-    document.querySelector(".ulist").appendChild(liEl)
-})
+$("button").click(function () {
+  let text = $("input").val();
+  let liEl = document.createElement("li");
+  liEl.innerHTML = `<i class="fa-solid fa-x fa-xs"></i>${text}`;
+  document.querySelector(".ulist").appendChild(liEl);
+  $("input").val("");
+});
 
 const list = document.querySelectorAll("ul");
-list.forEach(x => x.addEventListener('click', function(event){
-const element = event.target
-let iTag = element.tagName.toLowerCase()
-if (iTag == "i"){
-    element.parentNode.remove()
-}
-})
-)
-
+list.forEach((x) =>
+  x.addEventListener("click", function (event) {
+    const element = event.target;
+    let iTag = element.tagName.toLowerCase();
+    if (iTag == "i") {
+      element.parentNode.remove();
+    }
+  })
+);
 
 // $("li").click(function () {
-   
+
 //     // $(this).remove()
 
 //     // $(this).parent().remove()
 //     // $("li").eq(index).remove()
 //     alert("test")
 // });
-
