0e8983d654336506eb9ae1e26e0e9724b9cb23b9
Iyana Marquez <yayakix@gmail.com>
2022-05-17 19:18:36 -0400

    added local storage

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+// for (var i = 0; i < localStorage.length; i++) {
+//   $("ul").append(localStorage.getItem(localStorage.key(i)));
+// }
+let arr = []
+
+
 $("button").click(function () {
+  let ulEl = $("ul");
   let text = $("input").val();
+
   let liEl = document.createElement("li");
   liEl.innerHTML = `<i class="fa-solid fa-x fa-xs"></i>${text}`;
+  arr.push(liEl.textContent)
+  localStorage.setItem("listitems", arr);
+  // liEl.innerHTML = localStorage.getItem("text")
+  // localStorage.setItem(liEl, JSON.stringify(liEl));
+  // localStorage.getItem(liEl); 
+  // // ? JSON.parse(localStorage.getItem(liEl)) : "";
+  // console.log(localStorage);
+
   document.querySelector(".ulist").appendChild(liEl);
+
   $("input").val("");
+  console.log(localStorage.getItem("listitems"));
 });
 
 const list = document.querySelectorAll("ul");
